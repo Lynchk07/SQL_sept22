834581615f7f6a332af582ab09f6e791991d05b4
Lynchk07 <lynch.kristinj@gmail.com>
2022-09-26 16:57:21 -0700

    seeds and schema edit

--- db/schema.sql
+++ db/schema.sql
@@ -1,31 +1,38 @@
---create database --
+
 DROP DATABASE IF EXISTS department_db;
 CREATE DATABASE department_db;
 
---all of the following code will affect department_db --
+
 USE department_db;
 
---create the table --
 
-CREATE TABLE deparment (
-    --create a numeric column called "id" which will automatically increment its default value as we create new roles"
-    id INT PRIMARY KEY,
-    namex VARCHAR(30)
+
+CREATE TABLE department (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(30)
 );
 
 CREATE TABLE roles (
-    id: INT PRIMARY KEY
-    title: VARCHAR(30)
-    salary: DECIMAL
-    department_db: INT
+    id INT PRIMARY KEY auto_increment,
+    title VARCHAR(30),
+    salary DECIMAL,
+    department_id INT,
+    FOREIGN KEY (department_id)
+    REFERENCES department(id)
+    ON DELETE SET NULL
 
 );
 
-
 CREATE TABLE employee (
-    id: INT PRIMARY KEY
-    first_name: VARCHAR(30)
-    last_name: VARCHAR(30)
-    role_id: INT
-    manager_id: INT NOT NULL
+    id INT PRIMARY KEY auto_increment,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INT,
+    manager_id INT,
+    FOREIGN KEY (role_id)
+    REFERENCES roles(id)
+    ON DELETE SET NULL,
+    FOREIGN KEY (manager_id)
+    REFERENCES employee(id)
+    ON DELETE SET NULL
 );
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,30 +1,28 @@
---multiple departments--
-INSERT INTO department (id,namex)
+use department_db;
+INSERT INTO department (name)
 VALUES
-(001, 'Engineering'),
-(002, 'Finance'),
-(003, 'Legal' ),
-(004, 'Sales' );
+('Engineering'),
+('Finance'),
+('Legal' ),
+('Sales' );
 
 SELECT * FROM department;
 
-INSERT INTO roles (id,title,salary,department_db)
-VALUES ("801","Software Engineer","70000","Engineering")
+INSERT INTO roles (title,salary,department_id)
+VALUES ("Software Engineer","70000", 1),
+("Accountant","90000",2),
+("Lawyer","70000",3),
+("Account Manager","70000",4);
 
 SELECT * FROM roles;
 
---  id: INT PRIMARY KEY
---     title: VARCHAR(30)
---     salary: DECIMAL
---     department_db: INT
 
-INSERT INTO employee (id,first_name,last_name,role_id,manager_id)
-VALUES ("801","Dwight","Schutt","456"."null")
 
--- id: INT PRIMARY KEY
---     first_name: VARCHAR(30)
---     last_name: VARCHAR(30)
---     role_id: INT
---     manager_id: INT NOT NULL
+INSERT INTO employee (first_name,last_name,role_id,manager_id)
+VALUES ("Dwight","Schrutt",1,null),
+("Michael","Scott",2,null),
+("Andy","Bernard",3,2),
+("Jim","Halpert",4,2);
+
 
 SELECT * FROM employee;
